c21359c672d1edb802f656c0f6fad6e92485029b
unknown <shomirsaidov.abubakr@mail.ru>
2025-03-31 18:48:15 +0500

    inshaallah accepted 2

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -60,14 +60,14 @@
         <div @click="() => {
           this.$store.state.modalOn = true
           this.$store.state.chooseMode = 'start'
-        }" class="flex items-center space-x-3 px-3 py-3 border rounded-md text-sm shadow-sm  bg-white">
+        }" class="flex items-center space-x-3 px-3 py-3 border rounded-md text-sm shadow-sm  bg-white cursor-pointer">
           <img src="../assets/target.svg" alt="">
           <div class="w-full text-start">{{ this.$store.state.langLoaded.enter_address_from }}</div>
         </div>
         <div @click="() => {
           this.$store.state.modalOn = true
           this.$store.state.chooseMode = 'end'
-        }" class="flex items-center space-x-3 px-3 py-2 border rounded-md text-sm shadow-sm bg-white">
+        }" class="flex items-center space-x-3 px-3 py-2 border rounded-md text-sm shadow-sm bg-white cursor-pointer">
           <img src="../assets/location.svg" alt="">
           <div class="w-full text-start">{{ this.$store.state.langLoaded.enter_address_to }}</div>
 
