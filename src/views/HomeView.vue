8159d93ab4f981d284b96ed9fec093a858abe1d5
Shomirsaidov <abubakr.shomirsadov2@gmail.com>
2025-04-17 20:59:04 +0500

    sdfds

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -510,7 +510,7 @@ export default {
       this.$store.state.recentLocation = response.data.predefined_place_id_from;
 
       console.log(response); 
-      if(response.status == 'error') {
+      if(response.data.status == 'error') {
         this.infoModal = true;
       } else {
         this.$store.state.langLoaded = response.data.lang_text;
